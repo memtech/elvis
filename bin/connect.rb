b87016b219436303a0216033c09bf88344c35241
Daniel J. Pritchett <dpritchett@gmail.com>
2014-06-06 10:54:12 -0500

    Forced mute to work in irc

--- bin/connect.rb
+++ bin/connect.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 ENV["HUBOT_IRC_SERVER"]  = "irc.freenode.net"
+ENV["HUBOT_MUTE_ROOM_PREFIX"] = "" # weird irc hack
 ENV["HUBOT_IRC_ROOMS"]   ||= "#MemTechBot"
 ENV["HUBOT_IRC_NICK"]    ||= "MemtechBot"
 ENV["HUBOT_ALIAS"]       ||= "hubot"
