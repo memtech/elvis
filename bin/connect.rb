a993ba9e9a3d98ad80422e3bef543b27dea8ab21
Josh Lewis <josh.w.lewis@gmail.com>
2014-06-06 11:04:47 -0500

    MemtechBot is now Elvis :sunglasses:

--- bin/connect.rb
+++ bin/connect.rb
@@ -3,8 +3,7 @@
 ENV["HUBOT_IRC_SERVER"]  = "irc.freenode.net"
 ENV["HUBOT_MUTE_ROOM_PREFIX"] = "" # weird irc hack
 ENV["HUBOT_IRC_ROOMS"]   ||= "#MemTechBot"
-ENV["HUBOT_IRC_NICK"]    ||= "MemtechBot"
-ENV["HUBOT_ALIAS"]       ||= "hubot"
+ENV["HUBOT_IRC_NICK"]    ||= "Elvis"
 ENV["HUBOT_IRC_UNFLOOD"] ||= "true"
 
-system("bin/hubot -a irc --name #{ENV["HUBOT_IRC_NICK"]} --alias #{ENV["HUBOT_ALIAS"]}")
+system("bin/hubot -a irc --name #{ENV["HUBOT_IRC_NICK"]}")
